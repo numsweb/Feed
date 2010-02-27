20caa8c3002d51979ab94daaecd381291206a3de
John Kropka <jkropka@numsweb.com>
2010-02-27 13:56:52 -0600

    try to fix indicator

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,9 +4,9 @@ module ApplicationHelper
   
   def nav_li(link_text, path, confirm)
     if confirm == ""
-      "<li class='#{path == request.env["REQUEST_PATH"] ? "sel_link" : "unsel_link"}'>#{link_to(link_text, path)}</li>"
+      "<li class='#{path == request.path ? "sel_link" : "unsel_link"}'>#{link_to(link_text, path)}</li>"
     else
-      "<li class='#{path == request.env["REQUEST_PATH"] ? "sel_link" : "unsel_link"}'>#{link_to(link_text, path, confirm)}</li>"
+      "<li class='#{path == request.path ? "sel_link" : "unsel_link"}'>#{link_to(link_text, path, confirm)}</li>"
     end
   end
   
