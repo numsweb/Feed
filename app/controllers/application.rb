36eeeea3ba0f85fcd49754406f21ea6c744a0350
John Kropka <jkropka@numsweb.com>
2010-03-01 23:40:34 -0600

    more try

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -4,16 +4,16 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   #set session keep time
-  require 'aasm'
+  #require 'aasm'
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '572702701fc72a51f983f5d8586221a1'
   
-  def login_required
-    unless session[:user_id]
+  #def login_required
+  #  unless session[:user_id]
       #redirect_to '/login'
-    end
-  end
+  #  end
+  #end
   
 end
