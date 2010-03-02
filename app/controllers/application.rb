516ea64fca9f3ddfce0597fcddf01178b89c2878
John Kropka <jkropka@numsweb.com>
2010-03-02 00:24:12 -0600

    add before filter

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -10,10 +10,10 @@ class ApplicationController < ActionController::Base
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '572702701fc72a51f983f5d8586221a1'
   
-  #def login_required
-  #  unless session[:user_id]
-      #redirect_to '/login'
-  #  end
-  #end
+  def login_required
+    unless session[:user_id]
+      redirect_to '/login'
+    end
+  end
   
 end
