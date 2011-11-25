c0e575aebc6113fa6ef222511c05dc5c46830dbe
John Kropka <jkropka@mojoware.com>
2011-11-25 11:21:55 -0600

    create a feed initializer, add more feed URLs

---
+++ config/initializers/feed_urls.rb
@@ -0,0 +1,2 @@
+FEED_URLS = ["http://austin.craigslist.org/cpg/index.rss", "http://austin.craigslist.org/eng/index.rss", "http://austin.craigslist.org/sof/index.rss", "http://austin.craigslist.org/sad/index.rss", "http://austin.craigslist.org/web/index.rss", "https://www.elance.com/php/search/main/resultsproject.php?matchType=project&rss=1&matchKeywords=rails&catFilter=10183&statusFilter=10037&sortBy=timelistedSort&sortOrder=1", "http://www.guru.com/pro/ProjectResults.aspx?CID=102&BID=0&LOC=2"]
+ 
