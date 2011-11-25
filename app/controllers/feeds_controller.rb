8e1174e5b70c3fb3bf6b3460d8979036b558e1a8
John Kropka <jkropka@mojoware.com>
2011-11-25 04:09:58 -0600

    viewing read should not need to download feeds

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,22 +1,24 @@
 class FeedsController < ApplicationController
   
   def index
-    
-    feed_urls = ["http://austin.craigslist.org/cpg/index.rss", "http://austin.craigslist.org/eng/index.rss", "http://austin.craigslist.org/sof/index.rss", "http://austin.craigslist.org/sad/index.rss", "http://austin.craigslist.org/web/index.rss"]
-    @feeds = Feedzirra::Feed.fetch_and_parse(feed_urls)
-    
-    @feeds.each do |feed_url,listing|
-    
-      listing.entries.each do |entry|
-        feed=Feed.find_by_feed_url(entry.url)
-        if feed.blank?
-          feed=Feed.create(:title => entry.title, :feed_url => entry.url,
-                        :published => entry.published,
-                        :summary => entry.summary.sanitize)
+    unless params[:type] && params[:type] == "read"
+      #speed up the read display.
+
+      feed_urls = ["http://austin.craigslist.org/cpg/index.rss", "http://austin.craigslist.org/eng/index.rss", "http://austin.craigslist.org/sof/index.rss", "http://austin.craigslist.org/sad/index.rss", "http://austin.craigslist.org/web/index.rss"]
+      @feeds = Feedzirra::Feed.fetch_and_parse(feed_urls)
+      
+      @feeds.each do |feed_url,listing|
+      
+        listing.entries.each do |entry|
+          feed=Feed.find_by_feed_url(entry.url)
+          if feed.blank?
+            feed=Feed.create(:title => entry.title, :feed_url => entry.url,
+                          :published => entry.published,
+                          :summary => entry.summary.sanitize)
+          end
         end
       end
     end
- 
       if params[:type]
         if params[:type]=="all"
           @feeds = Feed.all
