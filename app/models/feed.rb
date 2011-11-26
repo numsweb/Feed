f89e08fd5fd6aa56e39f2abebc7793e821feae19
John Kropka <jkropka@mojoware.com>
2011-11-25 18:37:28 -0600

    sanitize all the data before displaying it

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,12 +1,13 @@
 class Feed < ActiveRecord::Base
   scope :unread, :conditions => ["is_read = ?", false]
   scope :read, :conditions => ["is_read = ?", true]
+  include ActionView::Helpers::SanitizeHelper
   
   def display_title
     if title.size < 51
-      return title
+      return sanitize(title)
     else
-      return title[0..50] + "..."
+      return sanitize(title[0..50]) + "..."
     end
   end
   
