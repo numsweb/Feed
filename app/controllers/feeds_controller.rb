d96866985fb54c174926d24ef2e959d7c68bd9c0
John Kropka <jkropka@mojoware.com>
2011-11-26 22:55:35 -0600

    change target URL

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -3,6 +3,7 @@ class FeedsController < ApplicationController
   def index
     unless params[:type] && params[:type] == "read" #speed up the read display.
       #feed_urls = ["http://austin.craigslist.org/cpg/index.rss", "http://austin.craigslist.org/eng/index.rss", "http://austin.craigslist.org/sof/index.rss", "http://austin.craigslist.org/sad/index.rss", "http://austin.craigslist.org/web/index.rss", "https://www.elance.com/php/search/main/resultsproject.php?matchType=project&rss=1&matchKeywords=rails&catFilter=10183&statusFilter=10037&sortBy=timelistedSort&sortOrder=1", "http://www.guru.com/pro/ProjectResults.aspx?CID=102&BID=0&LOC=2"]
+      Rails.logger.info "Fetching feeds ..."
       @feeds = Feedzirra::Feed.fetch_and_parse(FEED_URLS)
       
       @feeds.each do |feed_url,listing|
