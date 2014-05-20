29617a30a23763dc4c91951b369983f9a816fd2a
John Kropka <s2000.coder@gmail.com>
2014-05-19 19:22:17 -0500

    add the source before the title in the link list.

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -11,12 +11,12 @@ class FeedsController < ApplicationController
       FEED_URLS.each do |url|
         begin
           Timeout::timeout(30) do
-            feed = Feedjira::Feed.fetch_and_parse(url,{ :max_redirects => 3 })
+            feed = Feedjira::Feed.fetch_and_parse(url[0],{ :max_redirects => 3 })
             feed.entries.each do |entry|
               feed=Feed.find_by_feed_url(entry.url)
               if feed.blank?
                 begin
-                  feed=Feed.create(:title => entry.title, :feed_url => entry.url,
+                  feed=Feed.create(:title => url[1] + "::" + entry.title, :feed_url => entry.url,
                                    :published => entry.published,
                                    :summary => entry.summary.sanitize)
                 rescue
