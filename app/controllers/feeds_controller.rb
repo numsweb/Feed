3f12eb4e460cda53f3ba32a287b5dcd05d65584b
John Kropka <s2000.coder@gmail.com>
2014-05-22 11:23:05 -0500

    use delayed jobs, fix fetched array references, add button to launch delayed job and take fetching code out of controller

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -74,6 +74,10 @@ class FeedsController < ApplicationController
       end
     end
 
+  def feed_params
+    params.require(:feed).permit(:title, :published, :summary, :created_at, :is_read, :updated_at)
+  end
+
 =begin
     # Never trust parameters from the scary internet, only allow the white list through.
     def feed_params
