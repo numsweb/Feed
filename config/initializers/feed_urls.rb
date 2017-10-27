67d32fb4d5e40bd4565928ec9249e3f1ec092a27
numsweb <numsweb@users.noreply.github.com>
2017-10-27 11:05:51 -0400

    add more feed source URLs

--- config/initializers/feed_urls.rb
+++ config/initializers/feed_urls.rb
@@ -15,12 +15,17 @@ FEED_URLS = [
     ['ColumbiaCraigslist','http://columbia.craigslist.org/cpg/index.rss'],
     ['ColumbiaCraigslist','http://columbia.craigslist.org/sad/index.rss'],
 
+    ['AustinCraigslist', 'http://austin.craigslist.org/sof/index.rss'],
+    ['AustinCraigslist','http://austin.craigslist.org/web/index.rss'],
+    ['AustinCraigslist','http://austin.craigslist.org/cpg/index.rss'],
+    ['AustinCraigslist','http://austin.craigslist.org/sad/index.rss'],
+
+
+    ['RemoteRubyJobs', 'https://remoteok.io/remote-ruby-jobs.rss'],
+
     ['ELance','https://www.elance.com/php/search/main/resultsproject.php?matchType=project&rss=1&matchKeywords=rails&catFilter=10183&statusFilter=10037&sortBy=timelistedSort&sortOrder=1'],
     ['Guru', 'http://www.guru.com/pro/ProjectResults.aspx?CID=102&BID=0&LOC=2'],
     ['Dice', 'http://seeker.dice.com/jobsearch/servlet/JobSearch?op=301&N=0&Ntk=JobSearchRanking&Ntt=ruby+rails&Ntx=mode+matchall&Nty=1&Nf=p_Geocode|GCLT%2030.168175,-97.82291%2064.37376|p_PostedAge|LTEQ%201']
     ]
 
-#skip these for now, they are non-productive
-# "https://www.elance.com/php/search/main/resultsproject.php?matchType=project&rss=1&matchKeywords=rails&catFilter=10183&statusFilter=10037&sortBy=timelistedSort&sortOrder=1",
-# "http://www.guru.com/pro/ProjectResults.aspx?CID=102&BID=0&LOC=2",
 
