3ef62c2148f9c9bd8c630a4b2b91e548163bbf6a
numsweb <numsweb@users.noreply.github.com>
2017-10-27 11:05:51 -0400

    Rebase all the changes to include the new URLs and new ruby version
    freeze ruby version
    
    archive

--- config/initializers/feed_urls.rb
+++ config/initializers/feed_urls.rb
@@ -4,23 +4,36 @@ FEED_URLS = [
     ['AugustaCraigslist','http://augusta.craigslist.org/web/index.rss'],
     ['AugustaCraigslist','http://augusta.craigslist.org/cpg/index.rss'],
     ['AugustaCraigslist','http://augusta.craigslist.org/sad/index.rss'],
+
     ['CharlestonCraigslist', 'http://charleston.craigslist.org/eng/index.rss'],
     ['CharlestonCraigslist', 'http://charleston.craigslist.org/sof/index.rss'],
     ['CharlestonCraigslist','http://charleston.craigslist.org/web/index.rss'],
     ['CharlestonCraigslist','http://charleston.craigslist.org/cpg/index.rss'],
     ['CharlestonCraigslist','http://charleston.craigslist.org/sad/index.rss'],
+
     ['ColumbiaCraigslist', 'http://columbia.craigslist.org/eng/index.rss'],
     ['ColumbiaCraigslist', 'http://columbia.craigslist.org/sof/index.rss'],
     ['ColumbiaCraigslist','http://columbia.craigslist.org/web/index.rss'],
     ['ColumbiaCraigslist','http://columbia.craigslist.org/cpg/index.rss'],
     ['ColumbiaCraigslist','http://columbia.craigslist.org/sad/index.rss'],
+ 
+
+
+
+
+    ['RemoteRubyJobs', 'https://remoteok.io/remote-ruby-jobs.rss'],
 
     ['ELance','https://www.elance.com/php/search/main/resultsproject.php?matchType=project&rss=1&matchKeywords=rails&catFilter=10183&statusFilter=10037&sortBy=timelistedSort&sortOrder=1'],
     ['Guru', 'http://www.guru.com/pro/ProjectResults.aspx?CID=102&BID=0&LOC=2'],
     ['Dice', 'http://seeker.dice.com/jobsearch/servlet/JobSearch?op=301&N=0&Ntk=JobSearchRanking&Ntt=ruby+rails&Ntx=mode+matchall&Nty=1&Nf=p_Geocode|GCLT%2030.168175,-97.82291%2064.37376|p_PostedAge|LTEQ%201']
     ]
 
-#skip these for now, they are non-productive
-# "https://www.elance.com/php/search/main/resultsproject.php?matchType=project&rss=1&matchKeywords=rails&catFilter=10183&statusFilter=10037&sortBy=timelistedSort&sortOrder=1",
-# "http://www.guru.com/pro/ProjectResults.aspx?CID=102&BID=0&LOC=2",
 
+=begin
+
+    ['AustinCraigslist', 'http://austin.craigslist.org/sof/index.rss'],
+    ['AustinCraigslist','http://austin.craigslist.org/web/index.rss'],
+    ['AustinCraigslist','http://austin.craigslist.org/cpg/index.rss'],
+    ['AustinCraigslist','http://austin.craigslist.org/sad/index.rss'],
+
+=end
